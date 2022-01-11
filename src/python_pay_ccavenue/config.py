dfab93494a1d17930d392c63b101dbd463fb63b3
Kuldeep Pisda <22424149+kdpisda@users.noreply.github.com>
2022-01-11 06:12:02 +0530

    Basic project skeleton added

--- src/python_pay_ccavenue/config.py
+++ src/python_pay_ccavenue/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    WORKING_KEY = os.environ.get('CCAVENUE_WORKING_KEY')
-    ACCESS_CODE = os.environ.get('CCAVENUE_ACCESS_CODE')
-    MERCHANT_CODE = os.environ.get('CCAVENUE_MERCHANT_CODE')
+    WORKING_KEY = os.environ.get("CCAVENUE_WORKING_KEY")
+    ACCESS_CODE = os.environ.get("CCAVENUE_ACCESS_CODE")
+    MERCHANT_CODE = os.environ.get("CCAVENUE_MERCHANT_CODE")
--- src/python_pay_ccavenue/utils.py
+++ src/python_pay_ccavenue/utils.py
@@ -1,50 +1,56 @@
-from Crypto.Cipher import AES
 import hashlib
 import os
-from binascii import hexlify, unhexlify
+from binascii import hexlify
+from binascii import unhexlify
+
+from Crypto.Cipher import AES
+
 
 class CCAvenue:
     __WORKING_KEY: str = None
     __ACCESS_CODE: str = None
     __MERCHANT_CODE: str = None
-    __iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+    __iv = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
     __form_data = {
-        "order_id": '',
-        "currency": '',
-        "amount": '',
-        "redirect_url": '',
-        "cancel_url": '',
-        "language": '',
-        "billing_name": '',
-        "billing_address": '',
-        "billing_city": '',
-        "billing_state": '',
-        "billing_zip": '',
-        "billing_country": '',
-        "billing_tel": '',
-        "billing_email": '',
-        "delivery_name": '',
-        "delivery_address": '',
-        "delivery_city": '',
-        "delivery_state": '',
-        "delivery_zip": '',
-        "delivery_country": '',
-        "delivery_tel": '',
-        "merchant_param1": '',
-        "merchant_param2": '',
-        "merchant_param3": '',
-        "merchant_param4": '',
-        "merchant_param5": '',
-        "integration_type": '',
-        "promo_code": '',
-        "customer_identifier": '',
+        "order_id": "",
+        "currency": "",
+        "amount": "",
+        "redirect_url": "",
+        "cancel_url": "",
+        "language": "",
+        "billing_name": "",
+        "billing_address": "",
+        "billing_city": "",
+        "billing_state": "",
+        "billing_zip": "",
+        "billing_country": "",
+        "billing_tel": "",
+        "billing_email": "",
+        "delivery_name": "",
+        "delivery_address": "",
+        "delivery_city": "",
+        "delivery_state": "",
+        "delivery_zip": "",
+        "delivery_country": "",
+        "delivery_tel": "",
+        "merchant_param1": "",
+        "merchant_param2": "",
+        "merchant_param3": "",
+        "merchant_param4": "",
+        "merchant_param5": "",
+        "integration_type": "",
+        "promo_code": "",
+        "customer_identifier": "",
     }
 
-    __response_body = {
-        "encResp": ''
-    }
+    __response_body = {"encResp": ""}
 
-    def __init__(self, working_key: str = None, access_code: str = None, merchant_code: str = None) -> None:
+    def __init__(
+        self,
+        working_key: str = None,
+        access_code: str = None,
+        merchant_code: str = None,
+    ) -> None:
         """
         Initialize the CCAvenue class.
 
@@ -65,10 +71,13 @@ class CCAvenue:
         if working_key:
             self.__WORKING_KEY = working_key
         else:
-            self.__WORKING_KEY = os.environ.get('CCAVENUE_WORKING_KEY')
-        
+            self.__WORKING_KEY = os.environ.get("CCAVENUE_WORKING_KEY")
+
         if self.__WORKING_KEY is None or isinstance(self.__WORKING_KEY, str) is False:
-            raise ValueError('You must provide a working key for CCAvenue or set working key as an environment variable by setting it as CCAVENUE_WORKING_KEY.')
+            raise ValueError(
+                "You must provide a working key for CCAvenue or set working key as "
+                "an environment variable by setting it as CCAVENUE_WORKING_KEY."
+            )
 
     def load_access_code(self, access_code: str = None) -> None:
         """
@@ -79,10 +88,13 @@ class CCAvenue:
         if access_code:
             self.__ACCESS_CODE = access_code
         else:
-            self.__ACCESS_CODE = os.environ.get('CCAVENUE_ACCESS_CODE')
-        
+            self.__ACCESS_CODE = os.environ.get("CCAVENUE_ACCESS_CODE")
+
         if self.__ACCESS_CODE is None or isinstance(self.__ACCESS_CODE, str) is False:
-            raise ValueError('You must provide an access code for CCAvenue or set access code as an environment variable by setting it as CCAVENUE_ACCESS_CODE.')
+            raise ValueError(
+                "You must provide an access code for CCAvenue or set access code "
+                "as an environment variable by setting it as CCAVENUE_ACCESS_CODE."
+            )
 
     def load_merchant_code(self, merchant_code: str = None) -> None:
         """
@@ -93,10 +105,16 @@ class CCAvenue:
         if merchant_code:
             self.__MERCHANT_CODE = merchant_code
         else:
-            self.__MERCHANT_CODE = os.environ.get('CCAVENUE_MERCHANT_CODE')
-        
-        if self.__MERCHANT_CODE is None or isinstance(self.__MERCHANT_CODE, str) is False:
-            raise ValueError('You must provide a merchant code for CCAvenue or set merchant code as an environment variable by setting it as CCAVENUE_MERCHANT_CODE.')
+            self.__MERCHANT_CODE = os.environ.get("CCAVENUE_MERCHANT_CODE")
+
+        if (
+            self.__MERCHANT_CODE is None
+            or isinstance(self.__MERCHANT_CODE, str) is False
+        ):
+            raise ValueError(
+                "You must provide a merchant code for CCAvenue or set merchant "
+                "code as an environment variable by setting it as CCAVENUE_MERCHANT_CODE."
+            )
 
     def pad(self, data: str):
         """
@@ -105,7 +123,7 @@ class CCAvenue:
         :param data: The str data to be encrypted.
         """
         length = 16 - (len(data) % 16)
-        data += chr(length)*length
+        data += chr(length) * length
         return data
 
     def validate_request_body(self) -> None:
@@ -114,35 +132,56 @@ class CCAvenue:
 
         :param data: The request body to be validated.
         """
-        if self.__form_data['order_id'] is None or isinstance(self.__form_data['order_id'], str) is False:
-            raise ValueError('You must provide an order id for CCAvenue.')
-        
-        if self.__form_data['currency'] is None or isinstance(self.__form_data['currency'], str) is False:
-            raise ValueError('You must provide a currency for CCAvenue.')
-        
-        if self.__form_data['amount'] is None:
-            raise ValueError('You must provide an amount for CCAvenue.')
+        if (
+            self.__form_data["order_id"] is None
+            or isinstance(self.__form_data["order_id"], str) is False
+        ):
+            raise ValueError("You must provide an order id for CCAvenue.")
+
+        if (
+            self.__form_data["currency"] is None
+            or isinstance(self.__form_data["currency"], str) is False
+        ):
+            raise ValueError("You must provide a currency for CCAvenue.")
+
+        if self.__form_data["amount"] is None:
+            raise ValueError("You must provide an amount for CCAvenue.")
         else:
             try:
-                float(self.__form_data['amount'])
+                float(self.__form_data["amount"])
             except ValueError:
-                raise ValueError('You must provide a valid amount for CCAvenue.')
+                raise ValueError("You must provide a valid amount for CCAvenue.")
 
-        if self.__form_data['redirect_url'] is None or isinstance(self.__form_data['redirect_url'], str) is False:
-            raise ValueError('You must provide a valid redirect url for CCAvenue.')
-        
-        if self.__form_data['cancel_url'] is None or isinstance(self.__form_data['cancel_url'], str) is False:
-            raise ValueError('You must provide a valid cancel url for CCAvenue.')
-        
-        if self.__form_data['billing_name'] is None or isinstance(self.__form_data['billing_name'], str) is False:
-            raise ValueError('You must provide a billing name for CCAvenue.')
-        
-        if self.__form_data['billing_tel'] is None or isinstance(self.__form_data['billing_tel'], str) is False:
-            raise ValueError('You must provide a billing tel for CCAvenue.')
-        
-        if self.__form_data['billing_email'] is None or isinstance(self.__form_data['billing_email'], str) is False:
-            raise ValueError('You must provide a billing tel for CCAvenue.')
-    
+        if (
+            self.__form_data["redirect_url"] is None
+            or isinstance(self.__form_data["redirect_url"], str) is False
+        ):
+            raise ValueError("You must provide a valid redirect url for CCAvenue.")
+
+        if (
+            self.__form_data["cancel_url"] is None
+            or isinstance(self.__form_data["cancel_url"], str) is False
+        ):
+            raise ValueError("You must provide a valid cancel url for CCAvenue.")
+
+        if (
+            self.__form_data["billing_name"] is None
+            or isinstance(self.__form_data["billing_name"], str) is False
+        ):
+            raise ValueError("You must provide a billing name for CCAvenue.")
+
+        if (
+            self.__form_data["billing_tel"] is None
+            or isinstance(self.__form_data["billing_tel"], str) is False
+        ):
+            raise ValueError("You must provide a billing tel for CCAvenue.")
+
+        if (
+            self.__form_data["billing_email"] is None
+            or isinstance(self.__form_data["billing_email"], str) is False
+        ):
+            raise ValueError("You must provide a billing tel for CCAvenue.")
+
     def parse_request_body(self, request_body: dict) -> str:
         """
         Parse the request body to be encrypted.
@@ -151,13 +190,10 @@ class CCAvenue:
 
         :return: The request body in str to be encrypted.
         """
-        self.__form_data = {
-            **self.__form_data,
-            **request_body
-        }
+        self.__form_data = {**self.__form_data, **request_body}
         form_str = f"merchant_id={self.__MERCHANT_CODE}"
         for key, value in self.__form_data.items():
-            form_str += '&' + key + '=' + value
+            form_str += "&" + key + "=" + value
         return form_str
 
     def __get_cipher(self) -> AES:
@@ -168,12 +204,16 @@ class CCAvenue:
         """
         bytearrayWorkingKey = bytearray()
         bytearrayWorkingKey.extend(map(ord, self.__WORKING_KEY))
-        return AES.new(hashlib.md5(bytearrayWorkingKey).digest(), AES.MODE_CBC, self.__iv.encode("utf-8"))
-    
-    def encrypt(self, data: dict)-> bytearray:
+        return AES.new(
+            hashlib.md5(bytearrayWorkingKey).digest(),
+            AES.MODE_CBC,
+            self.__iv.encode("utf-8"),
+        )
+
+    def encrypt(self, data: dict) -> bytearray:
         """
         Encrypt the data to be sent to CCAvenue.
-            
+
         :param data: The data to be encrypted.
 
         :return: The encrypted data.
@@ -182,8 +222,8 @@ class CCAvenue:
         self.validate_request_body()
         data = self.pad(data)
         enc_cipher = self.__get_cipher()
-        return hexlify(enc_cipher.encrypt(data.encode("utf-8"))).decode('utf-8')
-    
+        return hexlify(enc_cipher.encrypt(data.encode("utf-8"))).decode("utf-8")
+
     def parse_response_body(self, response_body: str) -> str:
         """
         Parse the response body to be decrypted.
@@ -192,12 +232,15 @@ class CCAvenue:
 
         :return: The decrypted response body.
         """
-        if response_body.get("encResp") is None or isinstance(response_body.get("encResp"), str) is False:
-            raise ValueError('You must provide a valid response body for CCAvenue.')
+        if (
+            response_body.get("encResp") is None
+            or isinstance(response_body.get("encResp"), str) is False
+        ):
+            raise ValueError("You must provide a valid response body for CCAvenue.")
         else:
             self.__response_body = response_body
         return unhexlify(response_body.get("encResp"))
-    
+
     def decrypt(self, data: dict) -> str:
         """
         Decrypt the data received from CCAvenue.
@@ -208,5 +251,4 @@ class CCAvenue:
         """
         encryptedText = self.parse_response_body(data)
         dec_cipher = self.__get_cipher()
-        return dec_cipher.decrypt(encryptedText).decode('utf-8')
-
+        return dec_cipher.decrypt(encryptedText).decode("utf-8")
